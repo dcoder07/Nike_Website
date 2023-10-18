284b35b44358deda8c883b51c6e2ef9f2d9defec
Dishank Patil <dishankjp05@gmail.com>
2023-10-18 22:37:55 +0530

    fix nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,28 +6,27 @@ import { navLinks } from "../constants/index";
 
 const Nav = () => {
   return (
-    <header
-      className='padding-x py-8 z-10 absolute w-full text-xl'
-      
-    >
-      <div className='flex justify-between items-center max-container gap-16 '>
+    <header className='padding-x py-8 z-10 absolute w-full text-xl '>
+      <div className='flex justify-between items-center max-conatainer '>
         <a href='#link'>
           <img src={headerLogo} height={250} width={150} />
         </a>
-        <ul className='flex flex-1 justify-center align-items gap-16 font-light max-lg:hidden montserrat leading-normal text-gray-600 '>
+        <ul className='flex flex-1 justify-center align-items gap-8 xl:gap-16 font-light max-lg:hidden montserrat leading-normal text-gray-600 '>
           {navLinks.map((item, index) => (
             <li key={index}>
               <a href={item.href}>{item.label}</a>
             </li>
           ))}
         </ul>
-        <div className='mr-24'>
-          <span className='font-medium max-lg:hidden leading-normal'>
-            Sign in / Explore now
-          </span>
+        <div className='relative right-0'>
+          <button className='font-medium max-lg:hidden leading-normal'>
+            <a href='#signIn'>Sign in </a>
+            <span> / </span>
+            <a href='exploreNow'>Explore now </a>
+          </button>
         </div>
-        <div className='mr-12 cursor-pointer lg:hidden'>
-          <img src={hamburger} alt="hamburger" height='25' width='25' />
+        <div className='relative right-0 cursor-pointer lg:hidden'>
+          <img src={hamburger} alt='hamburger' height='25' width='25' />
         </div>
       </div>
     </header>
