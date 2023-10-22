9ac51b6fa8fd8e8302509062fb930c780a2760a2
Dishank Patil <dishankjp05@gmail.com>
2023-10-22 08:45:37 +0530

    sercivce card fix

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 
-
-const ServiceCard = ({ serviceIcon, content, label }) => {
+const ServiceCard = ({ imageIcon, content, label }) => {
   return (
     <div className='flex flex-col justify-center gap-5 rounded-2xl shadow-2xl p-10 '>
-      <div>
-        <img src={serviceIcon} alt="sevice-icons" className="h-5 w-5 "/>
+      <div className="w-auto">
+        <img src={imageIcon} alt='sevice-icons' className='h-12 w-12 rounded-full bg-[#FF6452] p-2 ' />
       </div>
       <span className='font-bold text-3xl'>{label}</span>
       <p className='text-lg leading-7 text-slate-500'>{content}</p>
--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -4,12 +4,12 @@ import ServiceCard from "../components/ServiceCard";
 
 const Services = () => {
   return (
-    <section className="xl:flex grid lg:grid-cols-2 grid-cols-1 gap-10"> 
+    <section className='xl:flex grid lg:grid-cols-2 grid-cols-1 gap-10'>
       {services.map((service, index) => {
         return (
           <ServiceCard
             key={index}
-            serviceIcon={service.imgURL}
+            imageIcon={service.imgURL}
             content={service.subtext}
             label={service.label}
           />
